17e32bc597db96792f808b96a28cc19fa87fd726
kieronberg <ikuria@edu.cdv.pl>
2024-06-13 19:35:18 +0200

    almost ready

---
+++ Assets/ball.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float ballSpeed;
+    public Vector2 LastVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        SendBallInRandomDirection();
+    }
+    public void SendBallInRandomDirection()
+    {
+        rigidbody2D.velocity = Vector3.zero;
+        rigidbody2D.isKinematic = true;
+        transform.position = Vector3.zero;
+        rigidbody2D.isKinematic = false;
+        rigidbody2D.velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * ballSpeed;
+        LastVelocity = rigidbody2D.velocity;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            SendBallInRandomDirection();
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rigidbody2D.velocity = Vector2.Reflect(LastVelocity, collision.contacts[0].normal);
+        LastVelocity = rigidbody2D.velocity;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(transform.position.x < 0)
+        {
+            Debug.Log("Right Player Scored");
+        }
+        if (transform.position.x > 0)
+        {
+            Debug.Log("Left Player Scored");
+        }
+        SendBallInRandomDirection();
+    }
+}
